25289ca42f803f94432cd2e2ffd84f0cf14e8bc7
Kai <wuvincentck505@gmail.com>
2024-03-15 17:18:20 +0800

    Add link paring

--- crawler.py
+++ crawler.py
@@ -1,22 +1,58 @@
 import aiohttp
 
 from bs4 import BeautifulSoup
+from charset_normalizer import detect
+from queue import Queue
+from urllib.parse import urlparse
+from urllib.parse import urljoin
 
 
 class Crawler:
 
     def __init__(self, arg_url):
         self.__arg_url = arg_url
+        self.__queue = Queue()
 
     async def main(self):
-        async with aiohttp.ClientSession() as client:
-            rawText = await self.fetch(client)
-            document = BeautifulSoup(rawText, "lxml")
-            for a in document.find_all('a'):
-                print(a, "\n")
+        # Put the request url into the queue.
+        self.__queue.put(self.__arg_url)
+
+        async with aiohttp.ClientSession(
+                fallback_charset_resolver=lambda r, b: detect(b)[
+                    'encoding'] or 'utf-8') as client:
+            while not self.__queue.empty():
+                await self.fetch(client)
 
     async def fetch(self, client):
-        async with client.get(self.__arg_url) as response:
-            print('<', response.status, '>')
-            assert response.status == 200
-            return await response.text()
+        headers = {
+            'Accept':
+            'text/html',
+            'Accept-Language':
+            'en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7',
+            'Dnt':
+            '1',
+            'User-Agent':
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) ' +
+            'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 ' +
+            'Safari/537.36'
+        }
+        url = self.__queue.get(False)
+        content = ''
+        async with client.get(url, headers=headers) as response:
+            print(response.status, url)
+            content = await response.text()
+
+        self.parse(url, content)
+
+        self.__queue.task_done()
+
+    def parse(self, url, raw_text):
+        document = BeautifulSoup(raw_text, 'lxml')
+        for a in document.find_all('a', href=True):
+            link = a['href'] if self.__is_absolute_path(
+                a['href']) else urljoin(url, a['href'])
+            text = a.text
+            print(link, text)
+
+    def __is_absolute_path(self, link) -> bool:
+        return bool(urlparse(link).netloc)
--- main.py
+++ main.py
@@ -2,4 +2,5 @@ import asyncio
 from crawler import Crawler
 
 crawler = Crawler('https://www.wenku8.net/novel/2/2763/index.htm')
+# crawler = Crawler('http://httpbin.org/get')
 asyncio.run(crawler.main())
